ca13227f8bb8fb4745029fbec5c85baaf693753e
Daniel Esteban <daespuor1991@gmail.com>
2021-01-10 16:41:39 -0500

    change default value

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,4 +2,5 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const BASE_URL = process.env.BASE_URL || "http://localhost:8888/api";
+export const BASE_URL =
+  process.env.BASE_URL || "https://daespuor-my-plan.netlify.app/api";
